76b501085946a2a0d7b4693d2508ae3923abb7c9
İlker <ilkersatur@gmail.com>
2022-11-12 15:21:49 +0300

    Add project files.

---
+++ 26.10.2022/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("selam");
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine("selam");
+Console.BackgroundColor = ConsoleColor.Red;
+Console.Clear();
+Console.SetCursorPosition(10, 15);
+Console.WriteLine("ilker");
---
+++ ConsoleKarYagdirma/Program.cs
@@ -0,0 +1,18 @@
+﻿void KarYağdır()
+{
+    Console.BackgroundColor = ConsoleColor.Gray;
+    Console.ForegroundColor = ConsoleColor.Black;
+    while (true)
+    {
+        Console.Clear();
+        for (int i = 0; i < 20; i++)
+        {
+            for (int j = 0; j < 100000000; j++) ;
+            Random rand = new Random();
+            Random rand1 = new Random();
+            Console.SetCursorPosition(rand.Next(1, 70), rand1.Next(1, 20));
+            Console.WriteLine("*");
+        }
+    }
+}
+KarYağdır();
